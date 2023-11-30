59e5c891b2d78702c857ba59627f1becdc7d67af
Jahidul Islam <jahidul.islam.sec@gmail.com>
2023-11-30 11:53:04 +0600

    solving issue in react router at build

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, '/client/dist')))
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.listen(5000, () => {
     console.log('Server is running!')
 })
+
